62f15238f79c92b931ce3a5a647fba897df6c392
Alex Vaos <alexvaos@apple.com>
2025-02-12 12:32:40 -0800

    Initial Commit

---
+++ Takeover/TakeoverApp.swift
@@ -0,0 +1,32 @@
+//
+//  TakeoverApp.swift
+//  Takeover
+//
+//  Created by Alex Vaos on 2/12/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TakeoverApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TakeoverTests/TakeoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  TakeoverTests.swift
+//  TakeoverTests
+//
+//  Created by Alex Vaos on 2/12/25.
+//
+
+import Testing
+@testable import Takeover
+
+struct TakeoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
